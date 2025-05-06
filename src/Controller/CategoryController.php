16cf9af2a27a6467aa9948603953eb033c1e8e0a
Araya RAKSACHOB <araya.raksachob@lapiscine.pro>
2025-05-06 14:28:39 +0200

    make:form

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,13 +3,44 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Form\CategoryForm;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 
 class CategoryController extends AbstractController{
+
+    #[Route('/create-category', name:"create-category")]
+    public function DisplayCreateCategory(Request $request, EntityManagerInterface $entityManager){
+        
+        //je crée une nouvelle instance de Category
+        $category = new Category();
+        
+        //créer le formulaire basé sur sur la classe CategoryForm 
+        //et le lie à l'instance $category
+        $categoryForm = $this->createForm(CategoryForm::class, $category);
+        
+        //handleRequest, méthode permettant de lier les données du formulaire à $categoryForm
+        //si l'user soumet le formulaire, les données seront intégrées dans l'entité $category
+        $categoryForm->handleRequest($request);
+
+        //Vérifier si le formulaire est soumis avec la méthode isSubmitted
+        if($categoryForm->isSubmitted()){
+
+            //sauvegarde les données en BDD
+            $entityManager->persist($category);
+            $entityManager->flush();
+        }
+
+        //le formulaire créé est passé à la vue avec createView
+        //permettant à Twig d'afficher le formulaire dans le template
+        return $this->render('create-category.html.twig', ['categoryForm' =>$categoryForm->createView()]);
+    }
 
     #[Route('/list-categories', name: 'categories')]
     public function DisplayCategories (CategoryRepository $CategoryRepository){
